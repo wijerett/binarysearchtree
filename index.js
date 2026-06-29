6bf65892c723a7556ae65e160c5b737891936710
wijerett <winnettjeremy@gmail.com>
2026-06-29 10:04:53 -0400

    start of class and functions

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+export class node {
+    constructor() {
+
+    }
+
+    tree(arr, root) {
+        arr = [];
+        root = this.buildTree(array);
+    }
+    buildTree(array) {
+        var array = array
+    }
+}
